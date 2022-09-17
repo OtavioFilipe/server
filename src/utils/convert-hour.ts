68f1d4fe35235e3746e1c399d75826d6b9caf4ed
OtavioFilipe <darksnack16@gmail.com>
2022-09-17 00:23:04 -0300

    config routes :rocket:

---
+++ src/utils/convert-hour.ts
@@ -0,0 +1,9 @@
+
+
+export function convertHour(hourString: string) {
+    const [hours, minutes] = hourString.split(':').map(Number)
+
+    const minutesAmount = (hours * 60) + minutes;
+
+    return minutesAmount;
+}
